400b346cc2f87b84adb10b13479782a704613932
IVxIV <software@ivxiv.com>
2015-01-24 20:48:09 -0600

    rest of the tv channels
    asdfgh

---
+++ HauntedHouse/Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(NetworkView))]
+public class NetworkManager : MonoBehaviour
+{
+	#region constants
+	
+	public enum eNetworkRole
+	{
+		NONE= -1,
+		PsychicServer= 0,
+		HauntedClient,
+	};
+	
+	#endregion
+	
+	#region data
+	
+	private eNetworkRole m_networkRole;
+	public eNetworkRole NetworkRole
+	{
+		get
+		{
+			return m_networkRole;
+		}
+	}
+	
+	#endregion
+	
+	#region methods
+	
+	void Awake()
+	{
+		m_networkRole= eNetworkRole.NONE;
+		
+		return;
+	}
+	
+	void Start()
+	{
+		return;
+	}
+	
+	void Update()
+	{
+		return;
+	}
+	
+	public void InitializeRole(eNetworkRole role)
+	{
+	
+	}
+	
+	public void DisposeRole()
+	{
+		m_networkRole= eNetworkRole.NONE;
+		
+		return;
+	}
+	
+	#endregion
+}
